34463a0b33f126b4e40a345135f0edfc8d033caf
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-11-18 10:44:42 +0100

    Aggiungere i file di progetto.

---
+++ quadrato/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace quadrato
+{
+    internal class Program
+    {
+
+        static bool Quadrato(int[] v)
+        {
+            for(int i=1; i<v.Length; i++)
+            {
+                if ((v[i-1] * v[i-1]) < v[i])
+                {
+                    return false;
+                }
+               
+            }
+
+            return true;
+            
+        }
+        static void Main(string[] args)
+        {
+            int[] vet = { 1, 2};
+
+           Console.WriteLine(Quadrato(vet));
+           
+        }
+    }
+}
